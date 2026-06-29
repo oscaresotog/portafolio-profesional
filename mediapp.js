daed7b8b1f18e6145da3db6ea477a2770e1d58a4
oscaresotog <oscar.e.soto.g.ing@gmail.com>
2026-06-28 23:56:49 -0600

    Se creo la pagina del proyecto Mediapp

---
+++ mediapp.js
@@ -0,0 +1,23 @@
+new Swiper(".miCarrusel",{
+
+    loop:true,
+
+    spaceBetween:30,
+
+    pagination:{
+
+        el:".swiper-pagination",
+
+        clickable:true
+
+    },
+
+    navigation:{
+
+        nextEl:".swiper-button-next",
+
+        prevEl:".swiper-button-prev"
+
+    }
+
+});
